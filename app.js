21ae6d8ff820b3b6a3dbc502c5701fc0ea2f2c98
Edgar Dorsey <dorsey.trevino@gmail.com>
2021-07-05 11:34:14 -0500

    Edition of code

--- app.js
+++ app.js
@@ -46,15 +46,17 @@ socialMedia.addEventListener('click', () => {
 //     rootMargin: '-140px 0px 0px 0px'
 // }
 
-// const sectionOneObserver = new IntersectionObserver(function (entries, sectionOneObserver) {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) {
-//             header.classList.add('nav-scrolled')
-//         } else {
-//             header.classList.remove('nav-scrolled')
-//         }
-//     })
-// }, sectionOneOptions);
+// const sectionOneObserver = new IntersectionObserver(
+//     function (entries, sectionOneObserver) {
+//         entries.forEach(entry => {
+//             if (!entry.isIntersecting) {
+//                 header.classList.add('nav-scrolled')
+//             } else {
+//                 header.classList.remove('nav-scrolled')
+//             }
+//         })
+//     }, sectionOneOptions
+// );
 
 // sectionOneObserver.observe(sectionOne)
 
